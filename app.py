37278992c8a3807ac2fe8d8c52a0b122191bfeaf
LopesSoh <sophiac.lopes2008@gmail.com>
2026-04-07 07:51:45 -0300

    metodo get feito

--- app.py
+++ app.py
@@ -4,3 +4,24 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+posts = [
+    {
+        "id": 1,
+        "titulo": "Meu primeiro post",
+        "resumo": "Resumo...",
+        "conteudo": "Conteúdo completo...",
+        "autor": "Carlos"
+    }
+]
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "posts": posts
+    })
